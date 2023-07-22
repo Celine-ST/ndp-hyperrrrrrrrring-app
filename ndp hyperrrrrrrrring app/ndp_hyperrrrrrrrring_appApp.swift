21a66bb9d65de729821e161b06a02100b44260d5
Celine-ST <celine.quek10@rafflesgirlssch.edu.sg>
2023-07-22 10:14:03 +0800

    Initial Commit

---
+++ ndp hyperrrrrrrrring app/ndp_hyperrrrrrrrring_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ndp_hyperrrrrrrrring_appApp.swift
+//  ndp hyperrrrrrrrring app
+//
+//  Created by Celine Quek on 22/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ndp_hyperrrrrrrrring_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
